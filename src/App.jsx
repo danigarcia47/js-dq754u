cab265e8a029d0fb640712fc56c540a2df5d0ed8
danigarcia47 <dgarcial01@educantabria.es>
2022-01-12 13:43:06 +0100

    Añadido metodo for en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,29 @@ import { Home } from '.components/Home';
 import { PaginaListas } from './components/PaginaListas';
 import { Perfil } from '.components/Perfil';
 import Header from '.components/Header';
+import {MenuItems} from './data/MenuItems';
+
+function createRoutes() {
+  const listRoutes = [];
+  for (let i = 0; i < MenuItems.length; i++) {
+    <Route path = {MenuItems[i].path} exact component={MenuItems[i].component} />
+  };
+  return listRoutes;
+}
 
 export function App() {
   return (
   <Router>
     <Header />
-    <Route path="/" exact component={Home} />
-    <Route path="/listas" component={PaginaListas} />
-    <Route path="/perfil" component={Perfil} />
+    {/* Metodo con For
+    {createRoutes()}
+    */}
+
+    {/* Metodo con map */}
+    {MenuItems.map(function createRoute(item) {
+      return (
+        <Route path = {item.path} exact component={item.component} />)
+    })}
   </Router>
   );
 }
